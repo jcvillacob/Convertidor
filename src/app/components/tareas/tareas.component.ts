bf3d4c8243fb1aa87fcc648e527066ca237c41d6
Juan camilo Villacob <jucaviza6@gmail.com>
2022-11-08 18:14:15 +0000

    Con los iconos en la lista

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -4,26 +4,27 @@ import { Tarea } from 'src/app/models/Tarea';
 @Component({
   selector: 'app-tareas',
   templateUrl: './tareas.component.html',
-  styleUrls: ['./tareas.component.css']
+  styleUrls: ['./tareas.component.css'],
 })
 export class TareasComponent implements OnInit {
   listTareas: Tarea[] = [];
-  nombreTarea = "";
+  nombreTarea = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  agregarTareas(){
+  agregarTarea() {
     // Crear un objeto tarea
-
+    const tarea: Tarea = {
+      nombre: this.nombreTarea,
+      estado: false,
+    };
 
     // Agregar el objeto Tarea al array
-
+    this.listTareas.push(tarea);
 
     // Resetear el formulario
-
-    
+    this.nombreTarea = '';
   }
 }
